0b62062f93bf502eb8b2d712f5c55e01fca259eb
Simon Riemertzon <simon.riemertzon@volvocars.com>
2018-03-14 16:54:27 +0100

    Add a simple singelton Datamangager and use it to populate ListView

---
+++ app/src/main/java/com/example/sriemert/androidskillportfolio/TapRecognizerActivity.java
@@ -0,0 +1,13 @@
+package com.example.sriemert.androidskillportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TapRecognizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap_recognizer);
+    }
+}
