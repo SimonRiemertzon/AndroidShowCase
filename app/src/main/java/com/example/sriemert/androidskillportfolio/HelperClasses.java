67ab4ed5429fd65aa0960ab26379e68b8e52cf4b
Simon Riemertzon <simon.riemertzon@gmail.com>
2018-03-19 19:37:56 +0100

    In the middle of implementing lambda as parameters to improve code

---
+++ app/src/main/java/com/example/sriemert/androidskillportfolio/HelperClasses.java
@@ -0,0 +1,32 @@
+package com.example.sriemert.androidskillportfolio;
+
+import static android.view.View.*;
+
+import android.content.DialogInterface;
+import android.view.View;
+
+/**
+ * Created by sirie1 on 2018-03-19.
+ */
+public class HelperClasses {
+
+	public class MyLovelyOnclickListener implements OnClickListener {
+
+		int myLovelyVariable;
+
+		public MyLovelyOnclickListener(int myLovelyVariable) {
+			this.myLovelyVariable = myLovelyVariable;
+		}
+
+		
+		@Override
+		public void onClick(View v)
+		{
+			int countValue = Integer.parseInt(mTextCounter.getText().toString());
+			mTextCounter.setText(String.valueOf(myLovelyVariable(countValue)));
+		}
+
+	};
+
+
+}
