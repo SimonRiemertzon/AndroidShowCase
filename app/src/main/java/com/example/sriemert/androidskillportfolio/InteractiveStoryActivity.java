53ffcb49f1ab264c4c13eba257016ec9832e4b96
Simon Riemertzon <simon.riemertzon@gmail.com>
2018-03-22 17:07:17 +0100

    Add some string resouces and change some styles

--- app/src/main/java/com/example/sriemert/androidskillportfolio/InteractiveStoryActivity.java
+++ app/src/main/java/com/example/sriemert/androidskillportfolio/InteractiveStoryActivity.java
@@ -9,6 +9,8 @@ public class InteractiveStoryActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_interactive_story);
-		Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show();
+
 	}
+
+
 }
