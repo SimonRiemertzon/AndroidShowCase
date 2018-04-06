fbf210122df9fd00861dd76d30fc0cac7ac2517e
Simon Riemertzon <simon.riemertzon@gmail.com>
2018-04-06 11:14:46 +0200

    Clean up code and use string resources for better stringhandeling

--- app/src/main/java/com/example/sriemert/androidskillportfolio/DataManager.java
+++ app/src/main/java/com/example/sriemert/androidskillportfolio/DataManager.java
@@ -1,43 +1,49 @@
 package com.example.sriemert.androidskillportfolio;
 
+import static javax.swing.UIManager.getString;
+
 import com.example.sriemert.androidskillportfolio.Demos.SkillList.SkillInfo;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 /**
  * Created by Jim.
  */
-
 public class DataManager {
-    private static DataManager ourInstance = null;
+	private static DataManager ourInstance = null;
+	private List<SkillInfo> mSkills = new ArrayList<>();
 
-    private List<SkillInfo> mSkills = new ArrayList<>();
+	public static DataManager getInstance() {
+		if (ourInstance == null) {
+			ourInstance = new DataManager();
+			ourInstance.initializeSkills();
+		}
+		return ourInstance;
+	}
 
-    public static DataManager getInstance() {
-        if (ourInstance == null) {
-            ourInstance = new DataManager();
-            ourInstance.initializeSkills();
-        }
-        return ourInstance;
-    }
+	//Adds a menuitem for the skill-list.
+	private void initializeSkills() {
+		mSkills.add(new SkillInfo(
+				0,
+				getString(R.string.skill0_title),
+				getString(R.string.skill0_description)));
+		mSkills.add(new SkillInfo(
+				1,
+				getString(R.string.skill1_title),
+				getString(R.string.skill1_description)));
+		mSkills.add(new SkillInfo(
+				2,
+				getString(R.string.skill2_title),
+				getString(R.string.skill2_description)
+		));
 
-    //Adds a menuitem for the skill-list.
-    private void initializeSkills() {
-        mSkills.add(new SkillInfo(
-                0,
-                "Taprecongnizers",
-                "Showcasing how to use taprecongnizers"));
-	    mSkills.add(new SkillInfo(
-			    1,
-			    "Interactive Story",
-			    "An interactive story where I learned several basic Java-programming elements"));
-	    }
+	}
 
-
-    public List<SkillInfo> getSkills() {
-        return mSkills;
-    }
-
+	public List<SkillInfo> getSkills() {
+		return mSkills;
+	}
 
 }
--- app/src/main/java/com/example/sriemert/androidskillportfolio/Demos/SkillList/SkillListActivity.java
+++ app/src/main/java/com/example/sriemert/androidskillportfolio/Demos/SkillList/SkillListActivity.java
@@ -13,78 +13,76 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.sriemert.androidskillportfolio.DataManager;
-import com.example.sriemert.androidskillportfolio.Demos.InteractiveStory.ui.InteractiveStoryActivity;
+import com.example.sriemert.androidskillportfolio.Demos.InteractiveStory.ui
+		.InteractiveStoryActivity;
 import com.example.sriemert.androidskillportfolio.Demos.TapRecognizerActivity;
 import com.example.sriemert.androidskillportfolio.R;
 
 import java.util.List;
 
 public class SkillListActivity extends AppCompatActivity {
-	private static final String TAG = "SkillListActivity" ;
+	private static final String TAG = "SkillListActivity";
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_skill_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_skill_list);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        initializeDisplayContent();
-    }
+		initializeDisplayContent();
+	}
 
-    private void initializeDisplayContent() {
-        final ListView listSkills = findViewById(R.id.list_skills);
-        final List<SkillInfo> skills = DataManager.getInstance().getSkills();
-        ArrayAdapter<SkillInfo> adapterSkills = new ArrayAdapter<SkillInfo>(this,
-                android.R.layout.simple_list_item_1, skills);
-        listSkills.setAdapter(adapterSkills);
+	private void initializeDisplayContent() {
+		final ListView listSkills = findViewById(R.id.list_skills);
+		final List<SkillInfo> skills = DataManager.getInstance().getSkills();
+		ArrayAdapter<SkillInfo> adapterSkills = new ArrayAdapter<SkillInfo>(this,
+				android.R.layout.simple_list_item_1, skills);
+		listSkills.setAdapter(adapterSkills);
 
-	    //Starts an activity based on the ID in the list.
-        listSkills.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            	Intent intent;
-            	switch (skills.get(i).getmSkillId()) {
-		            case 0:
-		            	intent = new Intent(SkillListActivity.this, TapRecognizerActivity.class);
-		            	break;
-		            case 1:
-		            	intent = new Intent(SkillListActivity.this, InteractiveStoryActivity.class);
+		//Starts an activity based on the ID in the list.
+		listSkills.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+				Intent intent;
+				switch (skills.get(i).getmSkillId()) {
+					case 0:
+						intent = new Intent(SkillListActivity.this, TapRecognizerActivity.class);
 						break;
-		            default:
-		            	intent = null;
-			            Log.wtf(TAG, "Intent is null. No content provided for menuitem.");
-	            }
-	            startActivity(intent);
+					case 1:
+						intent = new Intent(SkillListActivity.this, InteractiveStoryActivity
+								.class);
+						break;
+					default:
+						intent = null;
+						Log.wtf(TAG, "Intent is null. No content provided for menuitem.");
+				}
+				startActivity(intent);
 
+			}
 
+		});
+	}
 
-            }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_skills, menu);
+		return true;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-        });
-    }
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_skills, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 }
